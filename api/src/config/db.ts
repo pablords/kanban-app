320f90e482f01e9b654ef5e70a0dd4d2369aa0c2
Pablo ruan dos santos <pablords@gmail.com>
2022-08-27 11:37:00 -0300

    chore: setup config db

---
+++ api/src/config/db.ts
@@ -0,0 +1,32 @@
+import "reflect-metadata"
+import { DataSource } from "typeorm"
+import "dotenv/config"
+import { logger } from "./logger"
+
+declare type LoggerOptions = boolean | "all" | Array<("query" | "schema" | "error" | "warn" | "info" | "log" | "migration")>;
+
+export const connection = new DataSource({
+  type: process.env.DB_ENGINE,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  entities: [],
+  synchronize: true,
+  logging: process.env.DB_LOGGING as LoggerOptions,
+  timezone: process.env.TIMEZONE,
+  connectTimeout: 10000
+})
+
+connection.initialize().then(async (conn) => {
+  if (process.env.DB_LOGGING) {
+    logger.info(conn)
+  }
+}).catch((error) => {
+  if (process.env.NODE_ENV === "development") {
+    logger.error(error)
+  }
+})
+
+export const entityManager = connection.manager
